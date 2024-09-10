8055cb0e422ba964290872d0dc02ce941bcb2ec1
YUSRA SALEEM <159941826+Yusra-Saleem@users.noreply.github.com>
2024-09-10 18:06:41 +0500

    script.js

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
-// Get references to the toggle button and container
-var toggleBtn = document.getElementById('toggleBtn');
-var container = document.getElementById('container');
-if (toggleBtn && container) {
-    // Add an event listener to the toggle button
-    toggleBtn.addEventListener('click', function () {
-        // Toggle the 'active' class on the container element
-        container.classList.toggle('active');
-    });
-}
-else {
-    console.error("Elements not found");
-}
+document.addEventListener('DOMContentLoaded', function () {
+    var toggleBtn = document.getElementById('toggleBtn');
+    var hiddenSkills = document.getElementById('hiddenSkills');
+    console.log('Toggle Button:', toggleBtn);
+    console.log('Hidden Skills:', hiddenSkills);
+    if (toggleBtn && hiddenSkills) {
+        toggleBtn.addEventListener('click', function () {
+            console.log('Button Clicked');
+            hiddenSkills.classList.toggle('active');
+        });
+    }
+    else {
+        console.error("Elements not found");
+    }
+});
